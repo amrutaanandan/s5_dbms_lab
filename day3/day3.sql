36fc82cd214f407754054f2d911d80e152d22ff1
Amruta Anandan <76481299+amrutaanandan@users.noreply.github.com>
2023-02-11 17:16:01 +0530

    Update day3.sql

--- day3/day3.sql
+++ day3/day3.sql
@@ -10,4 +10,22 @@ alter table Client add constraint p_k primary key(Cno);
 alter table employee add constraint emp foreign key(Manager) references Employee(EmpId);
 alter table employee add constraint dep foreign key(DeptNo) references Department(DeptNo);
 
+/*Add a constraint to make the Job column unique*/
 
+alter table employee add constraint unique_job unique(job);
+
+/*Add constraints to check if Comm> 400 in the Employee table*/
+
+alter table employee add check(comm>400);
+
+/*Set Salary value by default as 25000, otherwise as the user enters*/
+
+alter table employee modify salary default 25000;
+
+/*Remove the constraint from the column Job*/
+
+alter table employee drop constraint unique_job;
+
+/*Display all the constraints*/
+
+select constraint_name, constraint_type from user_constraints;
