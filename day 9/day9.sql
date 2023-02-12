67affc6254db7ea76f6a2d60e0a345fa019a9d84
Amruta Anandan <76481299+amrutaanandan@users.noreply.github.com>
2023-02-12 18:26:00 +0530

    Update and rename day9_1.sql to day9.sql

--- day 9/day9.sql
+++ day 9/day9.sql
@@ -33,6 +33,37 @@ end hireemp;
 end emppack;
 /
 
+/*Create a package which contains a function to increase the commission of the employee*/
+
+create or replace package emppack as 
+function inccomm (eid 	employee.empid%type) return varchar2;
+end;
+/
+
+
+create or replace package body emppack as 
+function inccomm (eid 	employee.empid%type) return varchar2
+is
+newcomm employee.comm%type;
+	begin
+		select comm into newcomm from employee where empid = eid;
+		newcomm := newcomm + newcomm*0.5;
+		update employee set comm = newcomm where empid = eid;
+		return('Increased commission is: '||newcomm);
+	end inccomm;
+end emppack;
+/
+
+
+declare
+emid employee.empid%type := &emid;
+op varchar2(50);
+begin
+op := emppack.inccomm(emid);
+dbms_output.put_line(op);
+end;
+/
+
 declare
 eid1 	employee.empid%type := &eid1;
 ename1	employee.empname%type := &ename1;
